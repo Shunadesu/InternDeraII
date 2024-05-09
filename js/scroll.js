df0d844f179c40050aea5ff4654ae5d04f345b02
sonpa2002 <119403398+sonpa2002@users.noreply.github.com>
2024-05-09 16:52:13 +0700

    Delete js directory

--- js/scroll.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    var logo = document.querySelector('.nav__logo');
-    console.log(logo)
-    if (window.scrollY > 0) {
-        logo.classList.add('shrink');
-    } else {
-        logo.classList.remove('shrink');
-    }
-});
